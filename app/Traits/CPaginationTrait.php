a508db76b4df03a83709abd00ad827c83ace5447
Mostafa <ubirockteam@gmail.com>
2022-11-21 01:32:45 +0330

    custom pagination improvements

--- app/Traits/CPaginationTrait.php
+++ app/Traits/CPaginationTrait.php
@@ -10,6 +10,13 @@ trait CPaginationTrait
     public function scopeCpagination($query, $req, $apiResource = null)
     {
         $count = ceil($query->count() / ($req->perPage ?? 5)); // this has to be declared before continue of the query
+        $count==0 ? $count = 1 : null;
+        if($req->page && $req->page > $count) {
+            return response()->json([
+                'data' => [],
+                'message' => 'صفحه ی درخواستی از تعداد صفحات موجود بیشتر است'
+            ],400);
+        }
         $data = $query->offset((($req->page ?? 1) - 1) * ($req->perPage ?? 5))->limit(($req->perPage ?? 5))->get();
         $apiResource ? $data = $apiResource::collection($data) : null;
         return [
