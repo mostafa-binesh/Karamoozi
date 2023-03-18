804d476b523a24c27bf2921a078781b5918a5d02
Mostafa <ubirockteam@gmail.com>
2023-03-18 19:06:38 +0330

    resolve merge

--- app/Http/Resources/admin/CompanyResource.php
+++ app/Http/Resources/admin/CompanyResource.php
@@ -14,15 +14,16 @@ class CompanyResource extends JsonResource
      */
     public function toArray($request)
     {
-        return[
-            'id'=>$this->id,
-            'company_name'=>$this->company_name,
-            'caption'=>$this->caption,
-            'company_grade'=>$this->company_grade,
-            'company_boss_id'=>$this->company_boss_id,
-            // 'company_boss_id'=>$this->user->first_name, چرا نمیشه؟؟:((
-            'company_number'=>$this->company_number,
-            'company_registry'=>$this->company_registry_code,
+        // $this is company
+        return [
+            'id' => $this->id,
+            'company_name' => $this->company_name,
+            'caption' => $this->caption,
+            'company_grade' => $this->company_grade,
+            // 'company_boss_id' => $this->company_boss_id,
+            'company_boss_id' => $this->user->fullName(),
+            'company_number' => $this->company_number,
+            'company_registry' => $this->company_registry_code,
         ];
     }
 }
