4053cd13a5b2a2fcaf70fb08a14b84c0615b0e09
Mostafa <ubirockteam@gmail.com>
2023-04-14 14:45:35 +0430

    add term filter

---
+++ app/ModelFilters/Admin/TermFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\ModelFilters\Admin;
+
+use EloquentFilter\ModelFilter;
+
+class TermFilter extends ModelFilter
+{
+    /**
+     * Related Models that have ModelFilters as well as the method on the ModelFilter
+     * As [relationMethod => [input_key1, input_key2]].
+     *
+     * @var array
+     */
+    public $relations = [];
+    public function name($name)
+    {
+        $this->where('name', $name);
+    }
+}
